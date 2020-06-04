c6f2f5840977fc412b02410fafdd517919a5abad
Drona Banerjee <drona95.db@gmail.com>
2020-06-04 19:57:34 -0400

    Updates

---
+++ precache-manifest.47c15d77e1423f9d0c3ceb8a163d9d6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dce372c8244e116661040f0aa8e261a",
+    "url": "/todoListReact/index.html"
+  },
+  {
+    "revision": "217c33a6818b7101af32",
+    "url": "/todoListReact/static/css/main.735f4ce5.chunk.css"
+  },
+  {
+    "revision": "d8e0b5cb1c9c6c48e788",
+    "url": "/todoListReact/static/js/2.fb5eb7ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoListReact/static/js/2.fb5eb7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217c33a6818b7101af32",
+    "url": "/todoListReact/static/js/main.67ef1597.chunk.js"
+  },
+  {
+    "revision": "942b4537acd745b965a4",
+    "url": "/todoListReact/static/js/runtime-main.1a347df6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoListReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
